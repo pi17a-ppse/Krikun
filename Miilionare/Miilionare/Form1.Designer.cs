98ac2e68f06f91cc29060f8aee858fa6fd8a3350
Useless-guy <ogtacitus@gmail.com>
2021-02-25 10:42:15 +0300

    Added question and gain fields

--- Miilionare/Miilionare/Form1.Designer.cs
+++ Miilionare/Miilionare/Form1.Designer.cs
@@ -28,13 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Question = new System.Windows.Forms.Label();
+            this.Number = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Money = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Question);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 37);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(545, 154);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Вопрос";
+            // 
+            // Question
+            // 
+            this.Question.AutoSize = true;
+            this.Question.Location = new System.Drawing.Point(17, 32);
+            this.Question.Name = "Question";
+            this.Question.Size = new System.Drawing.Size(0, 20);
+            this.Question.TabIndex = 0;
+            // 
+            // Number
+            // 
+            this.Number.AutoSize = true;
+            this.Number.Location = new System.Drawing.Point(30, 9);
+            this.Number.Name = "Number";
+            this.Number.Size = new System.Drawing.Size(0, 20);
+            this.Number.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Money);
+            this.groupBox2.Location = new System.Drawing.Point(574, 37);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(176, 153);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Выигрышь";
+            // 
+            // Money
+            // 
+            this.Money.AutoSize = true;
+            this.Money.Location = new System.Drawing.Point(19, 29);
+            this.Money.Name = "Money";
+            this.Money.Size = new System.Drawing.Size(18, 20);
+            this.Money.TabIndex = 0;
+            this.Money.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(757, 345);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.Number);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Кто хочет стать миллионером?";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label Question;
+        private System.Windows.Forms.Label Number;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label Money;
     }
 }
 
